52d4972a4965750988372a7383c32d19a5f0866d
Maltstar <hftool@proton.me>
2024-02-14 14:18:56 +0100

    adding metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
 import Image from 'next/image'
 import App from './components/main/App'
-import Head from 'next/head'
-import Form from './components/UI_menu/Form'
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>My page title</title>
-      </Head>
       <App/>
     </>
 
